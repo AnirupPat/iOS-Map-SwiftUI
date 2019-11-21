7423eb5ce5f1d76240558154990e4451efa805be
Anirup Patnaik <anirup049@gmail.com>
2019-11-22 01:26:11 +0530

    first commit

---
+++ SwiftUI Demo/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  SwiftUI Demo
+//
+//  Created by Anirup Patnaik on 21/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUI Demo/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  SwiftUI Demo
+//
+//  Created by Anirup Patnaik on 22/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
